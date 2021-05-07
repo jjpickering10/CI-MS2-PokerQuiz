283f1cf86578603f48b3a169085c4828e212571b
jjpickering10 <jjpickering10@live.com>
2021-05-07 23:43:21 +0000

    Created club.js file and set out basic structure as well as questions array

---
+++ assets/js/club.js
@@ -0,0 +1,45 @@
+const question = document.getElementById('question')
+
+
+
+
+let questions = [
+  {
+    question: "true",
+    option1: "true",
+    option2: "false",
+    answer: 1
+  },
+  {
+    question: "false",
+    option1: "true",
+    option2: "false",
+    answer: 2
+  },
+  {
+    question: "true",
+    option1: "true",
+    option2: "false",
+    answer: 1
+  },
+  {
+    question: "false",
+    option1: "true",
+    option2: "false",
+    answer: 2
+  },
+  {
+    question: "true",
+    option1: "true",
+    option2: "false",
+    answer: 1
+  }
+];
+
+function startGame (){
+}
+
+
+function setQuestion (){
+
+}
