ac84da2fe3d86cbdbcd3f72f08fe05ffc981a8cc
jjpickering10 <jjpickering10@live.com>
2021-05-08 15:19:00 +0000

    Added in shuffle function, startbutton  and const variables

--- assets/js/club.js
+++ assets/js/club.js
@@ -1,11 +1,40 @@
-const question = document.getElementById('question')
+const firstOption = document.getElementById("first-option");
+const secondOption = document.getElementById("second-option");
+const startButton = document.getElementById("start");
+
+let shuffledRankings;
+let rankingIndex = 0
+
+startButton.addEventListener("click", startGame);
+
 
 
 function startGame (){
 }
 
+function shuffleRankings(array) {
+  // Used code from --- https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
-function setQuestion (){
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+
+function setRankings (){
 
 }
 
