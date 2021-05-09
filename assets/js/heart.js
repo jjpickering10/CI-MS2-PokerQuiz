5077e093aeb42e305e9d5dcc0fa6426762491460
jjpickering10 <jjpickering10@live.com>
2021-05-09 18:27:31 +0000

    Added display function to show shuffled cards

--- assets/js/heart.js
+++ assets/js/heart.js
@@ -1,13 +1,20 @@
 const handOne = document.getElementById('hand-one')
-const cardOne = document.getElementById('hand-two')
+const handTwo = document.getElementById('hand-two')
 const startButton = document.getElementById('start')
 
 // ---- Using API from https://deckofcardsapi.com/ -----
-
-const handRequest = new XMLHttpRequest();
+startButton.addEventListener('click', function(){
+    const handRequest = new XMLHttpRequest();
 handRequest.open('GET', 'https://deckofcardsapi.com/api/deck/new/draw/?count=2')
 handRequest.onload = function() {
     const handData = JSON.parse(handRequest.responseText)
     console.log(handData)
+    displayHands(handData)
 }
 handRequest.send();
+})
+
+function displayHands(data) {
+    handOne.innerText = data.cards[0].code //`${data.cards[0].value} ${data.cards[0].suit}`
+    handTwo.innerText = data.cards[1].code //`${data.cards[1].value} ${data.cards[1].suit}`
+}
