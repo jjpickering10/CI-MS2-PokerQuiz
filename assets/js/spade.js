fab3efad3b80b07ac459dc8f9d99069dffb52ae4
jjpickering10 <jjpickering10@live.com>
2021-05-11 19:06:24 +0000

    Added new function inside other function to get cards from deckID in initial XMLHttpRequest

--- assets/js/spade.js
+++ assets/js/spade.js
@@ -5,12 +5,26 @@ function getCardChoices() {
     let partialDeck;
     var ourRequest = new XMLHttpRequest();
     ourRequest.open("GET",
-    "https://deckofcardsapi.com/api/deck/new/shuffle/?cards=AS,2S,KS,AD,2D,QD,5C,8S,7S,JH,TD,9C");
+    "https://deckofcardsapi.com/api/deck/new/shuffle/?cards=AS,2S,KS,AD,2D,QD,5C,8C,7S,JH,0D,9C");
     ourRequest.onload = function () {
     partialDeck = JSON.parse(ourRequest.responseText)
     console.log(partialDeck)
+    getDeck = partialDeck.deck_id;
+    console.log(getDeck) // --- Returns deck ID, but not cards
+    drawDeck();
     }
     ourRequest.send();
-};
+
+    function drawDeck() {
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open("GET",
+    `https://deckofcardsapi.com/api/deck/${getDeck}/draw/?count=12`)
+    ourRequest.onload = function () {
+    gameDeck = JSON.parse(ourRequest.responseText)
+    console.log(gameDeck) // --- Returns cards from deck ID above
+    }
+    ourRequest.send();
+}
+}
 
 getCardChoices();
