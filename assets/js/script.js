cbd88922e7dd5387bcb969b670589cd38410c42d
jjpickering10 <jjpickering10@live.com>
2021-05-15 13:04:29 +0000

    Fix: removed bootstrap links in index.html as no longer needed. Edited modal layout and started function for each modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
+const openModals = document.querySelectorAll('[data-modal-target')
+const overlay = document.getElementById('overlay')
+
+openModals.forEach(e => {
+    e.addEventListener('click', () => {
+        const modal = document.querySelector(e.dataset.modalTarget)
+        console.log(modal)
+    })
+})
+
+
+
+
 document.querySelector('.toggle-button').addEventListener('click', () => {
     document.body.classList.toggle('dark')
 })
+
