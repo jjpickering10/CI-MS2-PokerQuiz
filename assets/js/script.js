2450e224742dd2d180375760adc04814cfdd9662
jjpickering10 <jjpickering10@live.com>
2021-05-15 13:28:26 +0000

    Fix: moved modals to end of index.html to fix positioning

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,18 @@
 const openModals = document.querySelectorAll('[data-modal-target')
-const overlay = document.getElementById('overlay')
+const overlay = document.getElementById('modal-overlay')
 
 openModals.forEach(e => {
     e.addEventListener('click', () => {
         const modal = document.querySelector(e.dataset.modalTarget)
         console.log(modal)
+        openModal(modal)
     })
 })
+
+function openModal(modal) {
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
 
 
 
