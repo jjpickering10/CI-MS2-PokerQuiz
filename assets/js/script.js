7de577e2c93e58b38e174c820b7f27bd23b04172
jjpickering10 <jjpickering10@live.com>
2021-05-15 14:06:05 +0000

    Added functions to close modal and edited overlay animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 const openModals = document.querySelectorAll('[data-modal-target')
+const closeModals = document.querySelectorAll('[data-close]')
 const overlay = document.getElementById('modal-overlay')
+const pageContainer = document.getElementById('page-container')
 
 openModals.forEach(e => {
     e.addEventListener('click', () => {
@@ -9,9 +11,33 @@ openModals.forEach(e => {
     })
 })
 
+closeModals.forEach(e => {
+    e.addEventListener('click', () => {
+        const modal = e.closest('.modal')
+        console.log(modal)
+        closeModal(modal)
+    })
+})
+
+overlay.addEventListener('click', () => {
+    const close = document.querySelectorAll('.modal.active')
+    close.forEach(e => {
+        closeModal(e);
+    })
+})
+
+
+
 function openModal(modal) {
     modal.classList.add('active')
     overlay.classList.add('active')
+    pageContainer.classList.add('not-active')
+}
+
+function closeModal(modal) {
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+    pageContainer.classList.remove('not-active')
 }
 
 
