81f552c4ec7e46ce90e03591b327b9072f40760a
jjpickering10 <jjpickering10@live.com>
2021-05-16 16:23:30 +0000

    Updated score.js to score localstorage items

--- assets/js/score.js
+++ assets/js/score.js
@@ -1,3 +1,17 @@
-const latestScore = localStorage.getItem("latest-score");
-const finalScore = document.getElementById("score-result");
-finalScore.innerHTML = latestScore;
+const latestDiamondScore = localStorage.getItem("latest-diamond-score");
+const latestClubScore = localStorage.getItem("latest-club-score");
+const latestHeartScore = localStorage.getItem("latest-heart-score");
+const latestSpadeScore = localStorage.getItem("latest-spade-score");
+
+const finalDiamondScore = document.getElementById("latest-diamond-score");
+const finalClubScore = document.getElementById("latest-club-score");
+const finalHeartScore = document.getElementById("latest-heart-score");
+const finalSpadeScore = document.getElementById("latest-spade-score");
+
+
+
+
+finalDiamondScore.innerText = latestDiamondScore;
+finalClubScore.innerText = latestClubScore;
+finalHeartScore.innerText = latestHeartScore;
+finalSpadeScore.innerText = latestSpadeScore;
