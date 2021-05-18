eca08f12c5a59f4e21349fe7ea7e4276eacbea91
jjpickering10 <jjpickering10@live.com>
2021-05-18 22:11:42 +0000

    Finished sendEmail function in sendemail.js and tested

--- assets/js/sendemail.js
+++ assets/js/sendemail.js
@@ -1,8 +1,23 @@
+
 function sendEmail(contact){
     emailjs.send("service_mbyy3ju","PokerQuiz", {
-        "from_name": contact.first-name.value,
-        "from_email": contactForm.email.value,
-        "message": contact.message.value,
+        from_name: contact.firstname.value,
+        from_email: contact.email.value,
+        message: contact.message.value
     })
+    .then(function (response) {
+                const form = document.getElementById('form');
+                form.classList.add('hide')
+                const sendEmailForm = document.getElementById('send-email-form')
+                const sent = document.createElement("p")
+                
+                sent.innerText = "Thanks for your message!"
+                sendEmailForm.appendChild(sent)
+                console.log('hello', response)
+            }),
+            function (error) {
+                alert("Oh No! There's been an error. Please refresh the page and fill out the form again", error);
+            };
+    
     return false;
 }
