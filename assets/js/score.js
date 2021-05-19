eca112ef8df33f892927d1182e846cb1cd179d0c
jjpickering10 <jjpickering10@live.com>
2021-05-19 16:27:29 +0000

    Fix: added guard clauses in score.js due to scores not showing because of localstorage null values

--- assets/js/score.js
+++ assets/js/score.js
@@ -8,27 +8,39 @@ const finalClubScore = document.getElementById("club-scores");
 const finalHeartScore = document.getElementById("heart-scores");
 const finalSpadeScore = document.getElementById("spade-scores");
 
-const diamondScores = JSON.parse(latestDiamondScore);
-const clubScores = JSON.parse(latestClubScore);
-const heartScores = JSON.parse(latestHeartScore);
-const spadeScores = JSON.parse(latestSpadeScore);
+const diamondScores = JSON.parse(latestDiamondScore) || [];
+const clubScores = JSON.parse(latestClubScore) || [];
+const heartScores = JSON.parse(latestHeartScore) || [];
+const spadeScores = JSON.parse(latestSpadeScore) || [];
 
 diamondScores.forEach((e) => {
+    if (diamondScores === null){
+        return;
+    }
   const score = document.createElement("p");
   score.innerText = `${e.name}: ${e.score} out of 5`;
   finalDiamondScore.appendChild(score);
 });
 clubScores.forEach((e) => {
+    if (clubScores === null){
+        return;
+    }
   const score = document.createElement("p");
   score.innerText = `${e.name}: ${e.score} out of 10`;
   finalClubScore.appendChild(score);
 });
 heartScores.forEach((e) => {
+    if (heartScores === null){
+        return;
+    }
   const score = document.createElement("p");
   score.innerText = `${e.name}: ${e.score} pair/s out of 10`;
   finalHeartScore.appendChild(score);
 });
 spadeScores.forEach((e) => {
+    if (spadeScores === null){
+        return;
+    }
   const score = document.createElement("p");
   score.innerText = `${e.name}: ${e.score} out of 5`;
   finalSpadeScore.appendChild(score);
